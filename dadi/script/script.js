c2f216cab7f94576eb47e6d41ce7465064b6d713
FedericaC03 <fdrk03@hotmail.it>
2020-08-25 00:47:52 +0200

    Esercizio dadi completato

--- dadi/script/script.js
+++ dadi/script/script.js
@@ -1,1 +1,14 @@
-// DADI
+// CHIEDO ALL'UTENTE DI INSERIRE UN NUMERO DA 1 A 6
+var numeroGiocatore = Math.floor(Math.random() * 6) + 1;
+console.log("il numero del giocatore è = " + numeroGiocatore);
+
+var numeroPc = Math.floor(Math.random() * 6) + 1;
+console.log("il numero del pc è = " + numeroPc);
+
+if (numeroGiocatore > numeroPc) {
+  alert("Complimenti hai vinto, il tuo numero è " + numeroGiocatore);
+} else if (numeroGiocatore < numeroPc) {
+  alert("Ritenta sarai più fortunato, il numero del pc è " + numeroPc);
+} else {
+  alert("Pareggio");
+}
